207b54fd848472ae61490f732be8d1236ea9ea5f
shane <avila.shane@yahoo.com>
2018-10-27 19:13:21 -0700

    Generates total solutions for the current n size

--- main/Main.java
+++ main/Main.java
@@ -5,16 +5,31 @@ import localsearch.ShotgunHillClimb;
 import nqueen.NQueenGenetic;
 import nqueen.NQueenShotgun;
 
+import java.util.*;
+
 public class Main {
+
+    private static int SOLUTION_CAP = 1000;
+
     public static void main(String[] args) {
         NQueenShotgun shotgunInit = new NQueenShotgun();
         ShotgunHillClimb hillClimb = new ShotgunHillClimb(shotgunInit);
         NQueenShotgun shotgunSol = hillClimb.search();
         System.out.println(shotgunSol);
 
-        NQueenGenetic geneticInit = new NQueenGenetic();
-        GeneticAlg genAlg = new GeneticAlg();
-        NQueenGenetic geneticSol = genAlg.search();
-        //System.out.println(geneticSol);
+        List<NQueenGenetic> solutionSet = new ArrayList<>();
+        int count = 0;
+        while (solutionSet.size() < SOLUTION_CAP && count < SOLUTION_CAP * 5) {
+            GeneticAlg genAlg = new GeneticAlg();
+            NQueenGenetic geneticSol = genAlg.search();
+            if (!solutionSet.contains(geneticSol) && geneticSol.getScore() == NQueenGenetic.goalScore())
+                solutionSet.add(geneticSol);
+            count++;
+        }
+        Collections.sort(solutionSet, new NQueenGenetic());
+        for (NQueenGenetic n : solutionSet)
+            System.out.println(n);
+        System.out.println(solutionSet.size());
+
     }
 }
