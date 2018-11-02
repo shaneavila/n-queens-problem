26efc92600224dff6fc34915d7815f762496c3b4
shane <avila.shane@yahoo.com>
2018-11-02 10:22:28 -0700

    Implemented methods to return multiple solutions for each local search algorithm

--- main/Main.java
+++ main/Main.java
@@ -8,28 +8,54 @@ import nqueen.NQueenShotgun;
 import java.util.*;
 
 public class Main {
-
-    private static int SOLUTION_CAP = 1000;
-
     public static void main(String[] args) {
-        NQueenShotgun shotgunInit = new NQueenShotgun();
-        ShotgunHillClimb hillClimb = new ShotgunHillClimb(shotgunInit);
+        ShotgunHillClimb hillClimb = new ShotgunHillClimb();
         NQueenShotgun shotgunSol = hillClimb.search();
-        System.out.println(shotgunSol);
+        System.out.println("Shotgun Hill Climbing: " + shotgunSol);
 
-        List<NQueenGenetic> solutionSet = new ArrayList<>();
+        GeneticAlg g = new GeneticAlg();
+        NQueenGenetic gSol = g.search();
+        System.out.println("\tGenetic Algorithm: "+gSol+"\n");
+
+//        generateShotgunSolutions();
+//        generateGeneticSolutions();
+    }
+
+    private static void generateShotgunSolutions() {
         int count = 0;
-        while (solutionSet.size() < SOLUTION_CAP && count < SOLUTION_CAP * 5) {
+        int solutionCap = 1000;
+        List<NQueenShotgun> solutionSet = new ArrayList<>();
+        while (solutionSet.size() < solutionCap && count < solutionCap) {
+            ShotgunHillClimb hillClimb = new ShotgunHillClimb();
+            NQueenShotgun shotgunSol = hillClimb.search();
+            if (!solutionSet.contains(shotgunSol) && shotgunSol.getScore() == NQueenShotgun.goalScore)
+                solutionSet.add(shotgunSol);
+            count++;
+        }
+        solutionSet.sort(new NQueenShotgun());
+        System.out.print("Shotgun Hill Climbing Solutions for ");
+        System.out.println(NQueenShotgun.SIZE+"x"+NQueenShotgun.SIZE);
+        for (NQueenShotgun n : solutionSet)
+            System.out.println(n);
+        System.out.println("Solutions found: " + solutionSet.size()+"\n");
+    }
+
+    private static void generateGeneticSolutions() {
+        int count = 0;
+        int solutionCap = 1000;
+        List<NQueenGenetic> solutionSet = new ArrayList<>();
+        while (solutionSet.size() < solutionCap && count < solutionCap) {
             GeneticAlg genAlg = new GeneticAlg();
             NQueenGenetic geneticSol = genAlg.search();
-            if (!solutionSet.contains(geneticSol) && geneticSol.getScore() == NQueenGenetic.goalScore())
+            if (!solutionSet.contains(geneticSol) && geneticSol.getScore() == NQueenGenetic.goalScore)
                 solutionSet.add(geneticSol);
             count++;
         }
-        Collections.sort(solutionSet, new NQueenGenetic());
+        solutionSet.sort(new NQueenGenetic());
+        System.out.print("Genetic Algorithm Solutions for ");
+        System.out.println(NQueenGenetic.SIZE+"x"+NQueenGenetic.SIZE);
         for (NQueenGenetic n : solutionSet)
             System.out.println(n);
-        System.out.println(solutionSet.size());
-
+        System.out.println("Solutions found: " + solutionSet.size()+"\n");
     }
 }
